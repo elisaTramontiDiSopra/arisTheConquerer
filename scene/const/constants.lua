5bf0a7b694ccc50757617945801291308eeba7a6
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2019-10-06 15:21:35 +0200

    Added new tiles and basic level settings

--- scene/const/constants.lua
+++ scene/const/constants.lua
@@ -49,6 +49,18 @@ local M = {
     { lvl = 1, timerSeconds = 5, pathTracerMoves = 200,
       obstaclesSrc = "scene/img/tiles/country/", obstacleTile = 'obstacle', treeTile = 'tree', pathTile = 'path',
       totalLevelTrees = 1, minPeeLevel = 5, maxPeeLevel = 100, peeStream = 6, vanishingPee = 2
+    },
+    { lvl = 2, timerSeconds = 5, pathTracerMoves = 100,
+      obstaclesSrc = "scene/img/tiles/pole/", obstacleTile = 'obstacle', treeTile = 'tree', pathTile = 'path',
+      totalLevelTrees = 1, minPeeLevel = 5, maxPeeLevel = 100, peeStream = 6, vanishingPee = 2
+    },
+    { lvl = 3, timerSeconds = 5, pathTracerMoves = 100,
+      obstaclesSrc = "scene/img/tiles/desert/", obstacleTile = 'obstacle', treeTile = 'tree', pathTile = 'path',
+      totalLevelTrees = 1, minPeeLevel = 5, maxPeeLevel = 100, peeStream = 6, vanishingPee = 2
+    },
+    { lvl = 4, timerSeconds = 5, pathTracerMoves = 100,
+      obstaclesSrc = "scene/img/tiles/isle/", obstacleTile = 'obstacle', treeTile = 'tree', pathTile = 'path',
+      totalLevelTrees = 1, minPeeLevel = 5, maxPeeLevel = 100, peeStream = 6, vanishingPee = 2
     }
   }
 }
