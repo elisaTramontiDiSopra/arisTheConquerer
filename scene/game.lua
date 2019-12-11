e5bdd4f7e3676403a8f685e71274be2843645cd4
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2019-12-11 16:38:28 +0100

    Delete hybrid mode

--- scene/game.lua
+++ scene/game.lua
@@ -464,7 +464,7 @@ function scene:create( event )
 	local sceneGroup = self.view
   physics.start()
   physics.setGravity(0,0)
-  physics.setDrawMode( "hybrid" )
+  --physics.setDrawMode( "hybrid" )
 
   -- init game vars
   initLevelSettings()
